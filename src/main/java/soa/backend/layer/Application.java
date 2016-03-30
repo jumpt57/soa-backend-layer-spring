03669da7ac9b674747a0971a44a14c72413ab79f
Julien <julien.maupetit@epsi.fr>
2016-03-30 15:12:35 +0200

    Suppression de Spring Security

--- src/main/java/soa/backend/layer/Application.java
+++ src/main/java/soa/backend/layer/Application.java
@@ -2,24 +2,12 @@ package soa.backend.layer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
-public class Application extends WebSecurityConfigurerAdapter {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) {
-		try {
-			http.csrf().disable().authorizeRequests()
-					.antMatchers("/css/**", "/js/**", "/img/**", "/fonts/**", "/reponse").permitAll();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 
 }
